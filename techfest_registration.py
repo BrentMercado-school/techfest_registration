36f2dfd23e9f2eaec2f3816030c95f1ebf216de0
Brent Mercado <brentallen.mercado@benilde.edu.ph>
2025-11-06 10:04:15 +0800

    Update: collection of participant's information

--- techfest_registration.py
+++ techfest_registration.py
@@ -11,32 +11,37 @@ while True:
         print("Please enter a valid number.\n")
         continue
 
-participants = {}
+participants = []
 for i in range(number_participant):
     name = input("Enter participant name: ")
     track = input("Enter chosen track: ")
-    participants[name] = track
+    participants.append({"name": name, "track": track})
 
 print("\nRegistered Participants:")
-counter = 1
-for name, track in participants.items():
-    print(f"{counter}. {name} - {track}")
-    counter += 1
+for i, participant in enumerate(participants):
+    print(f"{i+1}. {participant['name']} - {participant['track']}")
 
-track_count = {}
-for track in participants.values():
-    track_count[track] = track_count.get(track,0) + 1
+# track_count = {}
+# for participant in participants:
+#     track = participant['track']
+#     track_count[track] = track_count.get(track, 0) + 1
+#
+# unique_tracks = {track for track, count in track_count.items()}
+# print("\nTracks offered in this event:")
+# for track in unique_tracks:
+#     print(track)
+#
+# name_count = {}
+# for participant in participants:
+#     name = participant['name']
+#     name_count[name] = name_count.get(name, 0) + 1
+#
+# duplicated_names = {name for name, count in name_count.items() if count > 1}
+# print("\nDuplicated names found:")
+# for name in duplicated_names:
+#     print(name)
+#
+# print("\nParticipants per track:")
+# for track, count in track_count.items():
+#     print(f"{track}: {count}")
 
-unique_tracks = {track for track, count in track_count.items()}
-print("\nTracks offered in this event:")
-counter = 1
-for track in unique_tracks:
-    print(f"{counter}. {track}")
-    counter += 1
-
-
-# duplicate_names = {}
-
-print("\nParticipants per track:")
-for track, count in track_count.items():
-    print(f"{track}: {count}")
